df6e0baa7edaa91df7c1bf6e2c5f070ede843c38
tomas <tomas@Skada>
2025-09-21 14:39:41 -0300

    SAODKasokdas

--- FaltAPP/src/main/java/com/mycompany/faltapp/FaltAPP.java
+++ FaltAPP/src/main/java/com/mycompany/faltapp/FaltAPP.java
@@ -1,18 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.faltapp;
 
-/**
- *
- * @author tomas
- */
+
 public class FaltAPP {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("asdasdasdas");
+        int AOsdasodas=1;
              
     }
 }
